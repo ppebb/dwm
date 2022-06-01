1e4f7791decbae678262e134f97a5f6800474a66
pollen00 <ppeb@ppeb.me>
2022-06-01 02:10:12 -0500

    Apply doublepressquit patch

--- config.def.h
+++ config.def.h
@@ -29,6 +29,9 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+/* Lockfile */
+static char lockfile[] = "/tmp/dwm.lock";
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
