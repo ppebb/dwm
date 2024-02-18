891b90d062074f057b48a20660e3ab6a29ea1984
ppebb <ppeb@ppeb.me>
2024-02-17 19:10:15 -0600

    Fix movecenter to account for bar geometry

--- dwm.c
+++ dwm.c
@@ -1553,7 +1553,7 @@ void
 movecenter(const Arg *arg)
 {
 	selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
-	selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
+	selmon->sel->y = selmon->sel->mon->my + vertpad + bh + (selmon->sel->mon->mh - vertpad - bh - HEIGHT(selmon->sel)) / 2;
 	arrange(selmon);
 }
 
